4f57f78cae479f84a2d88c3612acbe97a65adf88
ZXC-zxc <xiaochen@ftsafe.com>
2019-09-25 12:00:42 +0800

    集成单例库

--- lib/jwallet_core.dart
+++ lib/jwallet_core.dart
@@ -1,7 +1,24 @@
 library jwallet_core;
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void init(){
+  getIt.registerSingleton<Calculator>(Calculator());
+}
+
+
+class Jwallet{
+  static Calculator getCalculator(){
+  return getIt<Calculator>();
+  }
+}
+
+
 
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  int addOne(int value){
+    return value + 1;
+  }
 }
